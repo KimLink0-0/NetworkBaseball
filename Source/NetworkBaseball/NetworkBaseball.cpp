16adec04675a6e55ad581172812786f4a881a1ae
KimLink <Linkedcirrus93@gmail.com>
2025-03-30 16:37:25 +0900

    1차 룰 및 변수 설정

--- Source/NetworkBaseball/NetworkBaseball.cpp
+++ Source/NetworkBaseball/NetworkBaseball.cpp
@@ -3,4 +3,6 @@
 #include "NetworkBaseball.h"
 #include "Modules/ModuleManager.h"
 
+DEFINE_LOG_CATEGORY(LogBaseBall);
+
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NetworkBaseball, "NetworkBaseball" );
